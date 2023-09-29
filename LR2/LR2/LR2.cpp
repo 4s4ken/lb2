29bc12e2ce63adefcd0be8d4ed6af1b42e1444c8
4skn <65232734+4s4ken@users.noreply.github.com>
2023-09-29 16:18:35 +0500

    Add files via upload

---
+++ LR2/LR2/LR2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	double a, b, c;
+	cin >> a >> b;
+	cout << "A+B = " << a + b << endl;
+	cout << "A-B = " << a - b << endl;
+	cout << "A*B = " << a * b << endl;
+	cout << "Middle of Num = " << (a + b) / 2 << endl;
+	if (a < 0)
+		a = -a;
+	if (b < 0)
+		b = -b;
+	c = b - a;
+	if (a > b)
+		c = a - b;
+	cout << "|Max| - |Min| = " << c << endl;
+	cout << "A/B = ";
+	printf("%.2f", a / b);
+}
